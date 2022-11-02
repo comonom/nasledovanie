728ec88116f83c9e863c04a74b7f00714d8621fa
LuntikLaElite <saneksivij@yandex.ru>
2022-11-02 18:50:27 +0400

    Добавьте файлы проекта.

---
+++ nasledovanie/Student.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace nasledovanie
+{
+    class Student
+    {
+        static void Main(string[] args)
+        { }
+        
+            public string Fio { get; set; }
+        public Student(string fio)
+        {
+            Fio = fio;
+        }
+        public void Print()
+        {
+            Console.WriteLine(Fio);
+        }
+    }
+
+    class Teacher : Student
+    {
+        public string Job { get; set; }
+        public Teacher(string fio, string job)
+            : base(fio)
+        {
+            Job=job;
+        }
+    }
+    }
+
